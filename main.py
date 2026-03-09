685519a67e54d0b9a41ed830fc1f6c0ec7f9b553
lilvld <vldborodi@yahoo.com>
2026-03-09 23:27:16 +0200

    Updatarea numelor si cantitatea produselor

--- main.py
+++ main.py
@@ -3,14 +3,10 @@ from product_manager import ProductManager
 
 manager = ProductManager()
 
-laptop = Product("Laptop", 649, 10)
-mouse = Product("Mouse", 14, 25)
+laptop = Product("Calculator", 1200, 3)
+mouse = Product("Mouse Wireless", 30, 15)
 tastatura = Product("Tastatura", 39, 30)
 
 manager.add_product(laptop)
 manager.add_product(mouse)
 manager.add_product(tastatura)
-
-manager.show_products()
-
-print(manager.total_value())
